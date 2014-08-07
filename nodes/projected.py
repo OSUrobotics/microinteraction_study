dacbf14dfbe00c3d32e654e990f2b99ce2813485
Dan Lazewatsky <dan@lazewatsky.com>
2014-08-07 12:11:03 -0700

    initial commit

---
+++ nodes/projected.py
@@ -0,0 +1,59 @@
+import roslib; roslib.load_manifest('projected_interface_builder')
+from projected_interface_builder.projected_interface import ProjectedInterface
+from projected_interface_builder.data_structures import PolygonInfo
+import rospy
+from vlc.srv import *
+
+class VideoControl(ProjectedInterface):
+    def __init__(self, polygon_file):
+        super(VideoControl, self).__init__(polygon_file)
+        self.play       = rospy.ServiceProxy('/play', Play)
+        self.pause      = rospy.ServiceProxy('/pause', Pause)
+        self.back10     = rospy.ServiceProxy('/back10', Back10)
+        self.forward10  = rospy.ServiceProxy('/forward10', Forward10)
+        self.vol_up     = rospy.ServiceProxy('/vol_up', VolUp)
+        self.vol_dn     = rospy.ServiceProxy('/vol_dn', VolDn)
+        self.mute       = rospy.ServiceProxy('/toggle_mute', MuteToggle)
+
+        self.register_callback('ff10',   self.cb_forward10)
+        self.register_callback('mute',   self.cb_mute)
+        self.register_callback('pause',  self.cb_pause)
+        self.register_callback('play',   self.cb_play)
+        self.register_callback('rew10',  self.cb_rew10)
+        self.register_callback('volup',  self.cb_volup)
+        self.register_callback('voldn',  self.cb_voldn)
+
+    def cb_forward10(self, poly):
+        self.forward10()
+
+    def cb_mute(self, poly):
+        self.mute()
+
+    def cb_pause(self, poly):
+        self.pause()
+
+    def cb_play(self, poly):
+        self.play()
+
+    def cb_rew10(self, poly):
+        self.back10()
+
+    def cb_volup(self, poly):
+        self.vol_up()
+
+    def cb_voldn(self, poly):
+        self.vol_dn()
+
+
+if __name__ == '__main__':
+    import sys
+    if len(rospy.myargv()) != 2:
+        print 'Usage: projected.py interface_file'
+        sys.exit(1)
+
+    interface_file = rospy.myargv()[1]
+    rospy.init_node('tv_interface')
+    interf = VideoControl(interface_file)
+    interf.start()
+    rospy.spin()
+    interf.maybe_write_changes()
---
+++ nodes/run_study.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env rosh
+import webbrowser
+import hashlib, time, random
+import rospy
+import numpy as np
+
+COND_PRACTICE   = 0
+COND_BASELINE   = 1
+COND_SCREEN     = 2
+COND_PROJECTED  = 3
+
+CONDITIONS = [1,2,3]
+
+VIDEO_DIR='~/videos'
+
+class VideoInfo:
+    def __init__(self, quiz_url, filename, duration):
+        self.quiz = quiz_url
+        self.filename = filename
+        self.duration = duration
+
+class Videos:
+    eu = VideoInfo(
+        'http://oregonstate.qualtrics.com/SE/?SID=SV_b89FnVhhBcavwZ7&subject_id=%s',
+        'eu.mp4',
+        rospy.Duration(350)
+        # rospy.Duration(5)
+    )
+    london1 = VideoInfo(
+        'http://oregonstate.qualtrics.com/SE/?SID=SV_a2Gew0tRFAMLUY5&subject_id=%s',
+        'london1.mp4',
+        rospy.Duration(287)
+        # rospy.Duration(5)
+    )    
+    london2 = VideoInfo(
+        'http://oregonstate.qualtrics.com/SE/?SID=SV_eP71O4yatU9m24l&subject_id=%s',
+        'london2.mp4',
+        rospy.Duration(347)
+        # rospy.Duration(5)
+    )
+    uk = VideoInfo(
+        'http://oregonstate.qualtrics.com/SE/?SID=SV_0PvRmY0tbwg2Vpj&subject_id=%s',
+        'uk.mp4',
+        rospy.Duration(314)
+        # rospy.Duration(5)
+
+    )
+
+COND_VIDEOS = {
+    1: Videos.uk,
+    2: Videos.eu,
+    3: Videos.london2
+}
+
+STIMULUS_SEQUENCE = [
+    services.forward10,
+    services.forward10,
+    services.forward10,
+    services.forward10,
+    services.forward10,
+    # services.pause,
+    # services.pause,
+    # services.pause,
+    # services.pause,
+    # services.pause,
+    services.toggle_mute,
+    services.toggle_mute,
+    services.toggle_mute,
+    services.toggle_mute,
+    services.toggle_mute,
+    lambda : (services.vol_dn() and services.vol_dn() and services.vol_dn() and services.vol_dn()),
+    lambda : (services.vol_dn() and services.vol_dn() and services.vol_dn() and services.vol_dn()),
+    lambda : (services.vol_dn() and services.vol_dn() and services.vol_dn() and services.vol_dn()),
+    lambda : (services.vol_dn() and services.vol_dn() and services.vol_dn() and services.vol_dn()),
+    lambda : (services.vol_dn() and services.vol_dn() and services.vol_dn() and services.vol_dn()),
+]
+SEQ_TITLES = [
+    'forward10',
+    'forward10',
+    'forward10',
+    'forward10',
+    'forward10',
+    'pause',
+    'pause',
+    'pause',
+    'pause',
+    'pause',
+    'mute',
+    'mute',
+    'mute',
+    'mute',
+    'mute',
+    'vol_dn',
+    'vol_dn',
+    'vol_dn',
+    'vol_dn',
+    'vol_dn',
+]
+
+def get_stimulus_sequence(video):
+    # set the seeed so we get "random" but consistent sequences
+    random.seed(video.duration.to_sec())
+    seq = zip(STIMULUS_SEQUENCE[:], SEQ_TITLES[:])
+    random.shuffle(seq)
+    # reverse so we can pop as we go
+    return list(reversed(zip(np.linspace(10,video.duration.to_sec()-10, len(STIMULUS_SEQUENCE)), seq)))
+
+def generate_subject_id():
+    return hashlib.md5(str(time.time())).hexdigest()[:10]
+
+def generate_sequence():
+    seq = CONDITIONS[:]
+    random.shuffle(seq)
+    return seq
+
+def run_study():
+    subject_id = generate_subject_id()
+
+    # run the practice video
+    vid = Videos.london1
+    services.start_video(os.path.join(VIDEO_DIR, vid.filename))
+    while topics.playback_time[0].data <= vid.duration:
+        pass
+
+    for condition in generate_sequence():
+        print 'starting condition', condition
+        if condition == COND_PROJECTED:
+            # show the projected interface
+            services.tv_interface.display_unmute()
+        else:
+            # TODO: make sure the web interface is showing
+            # rospy.Timer(rospy.Duration(0.0000001), lambda x: webbrowser.open(WEB_INTERFACE), oneshot=True)
+            pass
+
+        vid = COND_VIDEOS[condition]
+        stim_seq = get_stimulus_sequence(vid)
+        services.start_video(os.path.join(VIDEO_DIR, vid.filename))
+        # while topics.playback_time[0].data <= vid.duration:
+        stim_time, (stim, st) = stim_seq.pop()
+        for t in topics.playback_time[:]:
+            print t.data.to_sec(), 'of', vid.duration.to_sec()
+            # stimuli go here
+            if t.data.to_sec() >= stim_time:
+                print st
+                stim()
+                if stim_seq:
+                    stim_time, (stim, st) = stim_seq.pop()
+            if t.data >= vid.duration:
+                print 'done'
+                break
+
+        # hide the projected interface (regardless of if it's open)
+        services.tv_interface.display_mute()
+
+        print 'opening quiz'
+        # open up the quiz in the browser
+        rospy.Timer(rospy.Duration(0.0000001), lambda x: webbrowser.open(vid.quiz), oneshot=True)
+        # webbrowser.open(vid.quiz)
+        print 'quiz open, waiting for finish'
+        # quiz redirects to a local page that publishes an empty 
+        # message to let us know when the quiz has been completed
+        rospy.wait_for_message('/survey_done', msg.std_msgs.Empty)
+        print 'quiz done'
+
+run_study()
