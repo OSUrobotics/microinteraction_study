d3c009a7b60db13135edcf8b07a8782178805d29
Dan Lazewatsky <dan@lazewatsky.com>
2015-04-17 15:25:42 -0500

    basic sequence

--- nodes/run_study.py
+++ nodes/run_study.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env rosh
 import webbrowser
-import hashlib, time, random
+import hashlib
+import time
+import random
 import rospy
 import numpy as np
 import os
+from functools import partial
+
 
 COND_BASELINE   = 0
 COND_SCREEN     = 1
@@ -14,6 +18,7 @@ CONDITIONS = [1,2,3]
 
 VIDEO_DIR = '/home/lazewatd/videos'
 
+WEB_INTERFACE = 'file://' + os.path.join(packages.microinteraction_study.path, 'interfaces', 'web', 'simple.html')
 class VideoInfo:
     def __init__(self, quiz_url, filename, duration, instr_text='', pre=[], post=[]):
         self.quiz = quiz_url
@@ -40,33 +45,50 @@ class Videos:
         def __iter__(self):
             return (self[i] for i in xrange(len(self)))
 
-    eu = VideoInfo(
+    eu = VideoInfo(  # COND_PROJ_MOUSE
         'http://oregonstate.qualtrics.com/SE/?SID=SV_b89FnVhhBcavwZ7&subject_id=%s',
         'eu.mp4',
-        rospy.Duration(350)
+        rospy.Duration(350),
+        pre=[services.projected.display_unmute,
+             partial(packages.splashscreen.nodes.mouse_capture_py,
+                '_frame_id:=%s' % parameters.wall_frame_id,
+                node_name=parameters.mouse_capture_name())
+            ],
+        post=[partial(kill, nodes[parameters.mouse_capture_name()]),
+              services.projected.display_mute],
+        instr_text='COND_PROJ_MOUSE'
     )
-    london1 = VideoInfo(
+    london1 = VideoInfo(  # COND_BASELINE
         'http://oregonstate.qualtrics.com/SE/?SID=SV_a2Gew0tRFAMLUY5&subject_id=%s',
         'london1.mp4',
-        rospy.Duration(287)
+        rospy.Duration(287),
+        instr_text='COND_BASELINE'
     )
-    london2 = VideoInfo(
+    london2 = VideoInfo(  # COND_PROJ_GLASS
         'http://oregonstate.qualtrics.com/SE/?SID=SV_eP71O4yatU9m24l&subject_id=%s',
         'london2.mp4',
-        rospy.Duration(347)
+        rospy.Duration(347),
+        pre=[packages.glass_ros_bridge.nodes.start_glass,
+             services.projected.display_unmute,
+             partial(
+                packages.splashscreen.nodes.splashscreen_py(
+                    'hide_web',
+                    'Please use the projected inferface for this video')
+             )
+            ],
+        post=[packages.glass_ros_bridge.nodes.stop_glass,
+             services.projected.display_mute,
+             partial(kill, nodes.hide_web)],
+        instr_text='COND_PROJ_GLASS'
+
     )
-    uk = VideoInfo(
+    uk = VideoInfo(  # COND_SCREEN
         'http://oregonstate.qualtrics.com/SE/?SID=SV_0PvRmY0tbwg2Vpj&subject_id=%s',
         'uk.mp4',
-        rospy.Duration(314)
+        rospy.Duration(314),
+        pre=[partial(webbrowser.open, WEB_INTERFACE)],
+        instr_text='COND_SCREEN'
     )
-
-# COND_VIDEOS = {
-#     COND_BASELINE   : Videos.london1,
-#     COND_SCREEN     : Videos.uk,
-#     COND_PROJ_MOUSE : Videos.eu,
-#     COND_PROJ_GLASS : Videos.london2
-# }
 
 STIMULUS_SEQUENCE = [
     services.forward10,
@@ -128,8 +150,10 @@ def get_stimulus_sequence(video):
 def generate_subject_id():
     return hashlib.md5(str(time.time())).hexdigest()[:10]
 
-def generate_sequence():
-    return sample(list(enumerate(Videos)), len(Videos))
+def generate_sequence(subject_id):
+    # random.seed(subject_id)
+    # return random.sample(list(enumerate(Videos)), len(Videos))
+    return [(0, Videos[0])]
 
 def show_quiz(vid, subject_id):
     print 'opening quiz'
@@ -146,7 +170,7 @@ def run_study():
     subject_id = generate_subject_id()
 
     # have the user calibrate the glass frame
-    topics.splashscreen.message(
+    show_instructions_and_wait(
         'Please put on Google Glass.\n'
         'Note that you will not need to be able to read its screen.\n'
         'When you have it on comfortably, tap on the right stem to continue.\n'
@@ -156,12 +180,11 @@ def run_study():
     services.projected.display_mute()
 
     # wait for click
-    rospy.wait_for_message('/click', msg.std_msgs.Empty)
     show_instructions_and_wait(
         'Move your head so that the right edge of the Glass screen\n'
         'is just touching the right edge of the screen you\'re reading this on.\n'
         'When the edges are aligned, tap on the right stem to continue.\n')
-    packages.microinteraction_study.nodes.glass_offset_py('glass_adjust')
+    packages.microinteraction_study.nodes.glass_offset_py('glass_offset_node', 'glass_adjust')
 
     # now that glass is calibrated, start some practice
     topics.splashscreen.message(
@@ -199,36 +222,33 @@ def run_study():
     #     print topics.playback_time[0].data, vid.duration
     #     rate.sleep()
 
-    show_quiz(vid, subject_id)
+    # show_quiz(vid, subject_id)
     # make really sure the video is stopped
     # services.stop()
 
+    packages.glass_ros_bridge.nodes.stop_glass()
+
     print 'Starting conditions'
-    for condition, vid in generate_sequence():
+    for condition, vid in generate_sequence(subject_id):
         to_kill = []
         print 'starting condition', condition
-        if condition COND_PROJ_MOUSE:
-            # show the projected interface
-            services.projected.display_unmute()
-            to_kill.append(packages.splashscreen.nodes.mouse_capture_py(
-                '_frame_id:=%s' % wall_frame_id,
-                node_name=parameters.mouse_capture_node))
-        elif condition == COND_PROJ_GLASS:
-            services.projected.display_unmute()
-        elif condition == COND_SCREEN:
-            pass
-            # TODO: make sure the web interface is showing
-            # rospy.Timer(rospy.Duration(0.0000001), lambda x: webbrowser.open(WEB_INTERFACE), oneshot=True)
-
-        # vid = COND_VIDEOS[condition]
-        stim_seq = get_stimulus_sequence(vid)
         services.start_video(os.path.join(VIDEO_DIR, vid.filename))
         
+        # show the instructions
+
+        # run the prereqs
+        for c in vid.pre:
+            called = c()        
+
         # wait for vlc to be ready after starting the video
         while 'vlc_ready' not in parameters and not parameters.vlc_ready:
-            rospy.sleep(0.1)    
+            rospy.sleep(0.1)
 
         # while topics.playback_time[0].data <= vid.duration:
+        if condition != COND_BASELINE:
+            stim_seq = get_stimulus_sequence(vid)
+        else:
+            stim_seq = [(1000000000, (lambda : None, 'noop'))]
         stim_time, (stim, st) = stim_seq.pop()
         for t in topics.playback_time[:]:
             print t.data.to_sec(), 'of', vid.duration.to_sec()
@@ -241,14 +261,14 @@ def run_study():
             if t.data >= vid.duration:
                 print 'done'
                 break
+
         for n in to_kill: kill(n)
 
-        # make really sure the video is stopped
-        # services.stop()
+        # run the postreqs
+        for c in vid.post:
+            c()
 
-        # hide the projected interface (regardless of if it's open)
-        services.projected.display_mute()
-
+        # show the quiz
         show_quiz(vid, subject_id)
 
 run_study()
